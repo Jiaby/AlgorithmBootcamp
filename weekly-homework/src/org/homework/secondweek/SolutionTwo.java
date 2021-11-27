625a464a9b780d86d77cfeb8e4eb05ee2c0f05b1
jiaby <jiaby@liuyue.com>
2021-11-27 16:09:26 +0800

    week two work

--- weekly-homework/src/org/homework/secondweek/SolutionTwo.java
+++ weekly-homework/src/org/homework/secondweek/SolutionTwo.java
@@ -1,9 +1,131 @@
 package org.homework.secondweek;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * @author liuyue
  * @Description:
  * @date 2021/11/27 14:51
  */
 public class SolutionTwo {
+    /**
+     * 子域名访问次数统计
+     * 给定一个带访问次数和域名的组合，要求分别计算每个域名被访问的次数。其格式为访问次数+空格+地址，例如："9001 discuss.leetcode.com"。
+     * @link https://leetcode-cn.com/problems/subdomain-visit-count/
+     */
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String cpdomain : cpdomains) {
+            String[] countDomain = cpdomain.split(" ");
+            int count = Integer.parseInt(countDomain[0]);
+            String[] domains = splitDomain(countDomain[1]);
+            for (String domain : domains) {
+                if (map.containsKey(domain)) {
+                    map.put(domain, map.get(domain) + count);
+                } else {
+                    map.put(domain, count);
+                }
+            }
+        }
+        ArrayList<String> list = new ArrayList<>();
+        map.forEach((k,v) -> list.add(v + " " + k));
+        return list;
+    }
+
+    private String[] splitDomain(String domain) {
+        String[] split = domain.split("\\.");
+        String[] s = new String[split.length];
+        StringBuilder preStr = new StringBuilder();
+        for (int i = split.length - 1; i >= 0; i--) {
+            String string = preStr.insert(0, split[i]).toString();
+            s[i] = string;
+            preStr.insert(0, ".");
+        }
+        return s;
+    }
+
+    /**
+     * 数组的度
+     * 给定一个非空且只包含非负数的整数数组 nums，数组的度的定义是指数组里任一元素出现频数的最大值。
+     *
+     * 你的任务是在 nums 中找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
+     * @link https://leetcode-cn.com/problems/degree-of-an-array/
+     */
+    public int findShortestSubArray(int[] nums) {
+        // 求出数组的度，即任一元素出现频数的最大值
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        // 哪些数字出现的频次最大
+        Map<Integer, Integer> max = new HashMap<>();
+        map.forEach((k1, v1) -> {
+            Iterator<Map.Entry<Integer, Integer>> iterator = max.entrySet().iterator();
+            if (iterator.hasNext()) {
+                Map.Entry<Integer, Integer> next = iterator.next();
+                if (next.getValue() < v1) {
+                    max.clear();
+                    max.put(k1,v1);
+                } if (next.getValue().equals(v1)) {
+                    max.put(k1,v1);
+                }
+            } else {
+                max.put(k1,v1);
+            }
+        });
+        IntValue result = new IntValue();
+        result.value = nums.length;
+        max.forEach((k,v) -> {
+            int start = 0; int end = nums.length - 1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == k) {
+                    start = i;
+                    break;
+                }
+            }
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (nums[i] == k) {
+                    end = i;
+                    break;
+                }
+            }
+            result.value = Math.min(result.value, end - start + 1);
+        });
+        return result.value;
+    }
+
+    class IntValue {
+        int value;
+    }
+
+    private int findDegree(int[] nums, int start, int end) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = start; i <= end; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        int max = 0;
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> next = iterator.next();
+            max = Math.max(max, next.getValue());
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        SolutionTwo solutionTwo = new SolutionTwo();
+        System.out.println(solutionTwo.findShortestSubArray(new int[] {6,5,5}));
+    }
+
 }
