6ae206391a1c9706090eb194c21f022222d8153a
jiaby <492435707@qq.com>
2021-11-30 19:10:47 +0800

    二维数组求和，差分

--- weekly-homework/src/org/train/secondweek/test/ClassTestTwo.java
+++ weekly-homework/src/org/train/secondweek/test/ClassTestTwo.java
@@ -1,6 +1,7 @@
 package org.train.secondweek.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,8 +46,56 @@ public class ClassTestTwo {
         return mapA.equals(wordsMap);
     }
 
+
     public static void main(String[] args) {
         ClassTestTwo classTestTwo = new ClassTestTwo();
-        classTestTwo.findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake", new String[]{"fooo","barr","wing","ding","wing"});
+        int[][] bookings = new int[][]{{1,2,10},{2,3,20},{2,5,25}};
+        classTestTwo.corpFlightBookings(bookings, 5);
+    }
+
+    /**
+     * 航班预计统计
+     * @link https://leetcode-cn.com/problems/corporate-flight-bookings/
+     */
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] delta = new int[n + 2];
+        Arrays.fill(delta, 0);
+        // 原数组字段l~r的加、减一个数，等于其差分数组下标l处加、减一个数和r+1处减、加一个数
+        for (int[] booking : bookings) {
+            int first = booking[0], last = booking[1], seats = booking[2];
+            delta[first] = delta[first] + seats;
+            delta[last + 1] = delta[last + 1] - seats;
+        }
+        int[] sum = new int[n];
+        sum[0] = delta[1];
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = delta[i + 1] + sum[i - 1];
+        }
+        return sum;
+    }
+}
+
+/**
+ * @link https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+ * 二维区域和检索 - 矩阵不可变
+ */
+class NumMatrix {
+    int[][] s;
+
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        s = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // 二维数组前缀和公式
+                s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        row1 ++; col1 ++; row2 ++; col2 ++;
+        return s[row2][col2] - s[row2][col1 - 1] - s[row1 - 1][col2] + s[row1 - 1][col1 - 1];
     }
 }
