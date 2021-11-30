143dee28157b3d905cde62340e2c179edc33c269
jiaby <492435707@qq.com>
2021-11-30 20:37:49 +0800

    ..

--- weekly-homework/src/org/train/secondweek/WeeklyWork.java
+++ weekly-homework/src/org/train/secondweek/WeeklyWork.java
@@ -1,9 +1,41 @@
 package org.train.secondweek;
 
+import java.util.List;
+
 /**
  * @author liuyue
  * @Description: 周常
  * @date 2021/11/24 22:17
  */
 public class WeeklyWork {
+
+    /**
+     * @Link https://leetcode-cn.com/problems/subdomain-visit-count/
+     */
+    public List<String> subdomainVisits(String[] cpdomains) {
+        return null;
+    }
+
+    /**
+     * @Link https://leetcode-cn.com/problems/degree-of-an-array/
+     */
+    public int findShortestSubArray(int[] nums) {
+        return -1;
+    }
+
+    /**
+     * @Link https://leetcode-cn.com/problems/number-of-submatrices-that-sum-to-target/
+     */
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        return -1;
+
+    }
+
+    /**
+     * @link https://leetcode-cn.com/problems/subarray-sum-equals-k/
+     */
+    public int subarraySum(int[] nums, int k) {
+        return -1;
+
+    }
 }
