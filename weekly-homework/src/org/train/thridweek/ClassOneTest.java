29cd3ab98f681d09a158016c54665fdb7f62d78f
jiaby <492435707@qq.com>
2021-12-01 19:59:49 +0800

    addOneTest

--- weekly-homework/src/org/train/thridweek/ClassOneTest.java
+++ weekly-homework/src/org/train/thridweek/ClassOneTest.java
@@ -1,23 +1,29 @@
 package org.train.thridweek;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ClassOneTest {
+    List<List<Integer>> ans = new ArrayList<>();
     /**
      * 子集
      * @link  https://leetcode-cn.com/problems/subsets/
      */
     public List<List<Integer>> subsets(int[] nums) {
-
+        subsetsHelp(nums, 0, new LinkedList<>());
+        return ans;
     }
 
-    private void subsetsHelp(int[] nums, int idx, List<List<Integer>> list) {
-        if (idx == nums.length - 1) {
-            list.add(Arrays.asList(nums[idx]));
+    private void subsetsHelp(int[] nums, int idx, LinkedList<Integer> list) {
+        if (idx == nums.length) {
+            ans.add(list);
             return;
         }
         subsetsHelp(nums, idx + 1, list);
-
+        list.push(nums[idx]);
+        subsetsHelp(nums, idx + 1, list);
+        list.pop();
     }
 }
